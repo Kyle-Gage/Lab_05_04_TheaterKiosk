0e181e3f86e42528dd0e08eec3e1f1fc34a71a02
Kyle-Gage <gageka@mail.uc.edu>
2022-02-09 12:07:08 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	/*
+	As people pass through an entry kiosk at the theater, they are prompted to enter their age.
+	If they are 21 or older, they get a paper wrist band.
+	Code a Java program that asks the user to enter their age and then if they are 21 or over displays a message that they get a wrist band.
+	(Note that the program does nothing if they are not 21 or over…)
+	 */
+
+        int age = 15;
+
+        if (age >= 21)
+        {
+            System.out.println("You get a wrist band!");
+        }
+    }
+}
